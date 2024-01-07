a5f24f016552a6621d217990e82452695bab5785
Iasam <115777337+IasamKaKaKhail@users.noreply.github.com>
2024-01-07 15:23:52 +0500

    Update run_this_script_on_successful_workflow_deployment.sh

--- run_this_script_on_successful_workflow_deployment.sh
+++ run_this_script_on_successful_workflow_deployment.sh
@@ -8,6 +8,7 @@ mv * App
 cd App
 mv Dockerfile docker-compose.yaml bash_script_for_removing_running_containers.sh ..
 cd ..
+sudo chmod 0777 bash_script_for_removing_running_containers.sh 
 sudo snap install docker
 sudo docker build -t app .
 sudo docker compose up -d
